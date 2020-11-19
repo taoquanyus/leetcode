50d4558c3767c5edb0d4e91cbc6d3b00af0daf9f
Quanyu <taoquanyus@foxmail.com>
2020-11-19 15:35:09 +0100

    new present

---
+++ test/src/test.java
@@ -0,0 +1,11 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class test {
+    public static void main(String[] args) {
+        int a=2,b=3;
+        System.out.println((float) b/a);
+
+    }
+}
