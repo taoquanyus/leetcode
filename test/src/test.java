0445d06e9f1b8fc564bf75a8970768a31184f61d
Quanyu <taoquanyus@foxmail.com>
2020-12-01 21:03:51 +0100

    新手70题之算法 回溯法（递归部分为完成）

--- test/src/test.java
+++ test/src/test.java
@@ -4,8 +4,13 @@ import java.util.Map;
 
 public class test {
     public static void main(String[] args) {
-        int a=2,b=3;
-        System.out.println((float) b/a);
+        int a=22,b=3;
+        String c=Integer.toString(a)+'b'+(b);//一个函数内没有用，或者；隔开，相当于用同一个函数？？
+        int[] sum_value=new int[10];
+        sum_value[2]=5;
+        {
+            System.out.println(sum_value[3]);
+        }
 
     }
 }
