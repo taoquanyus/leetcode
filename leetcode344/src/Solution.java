871e74242ae3d8c1b7fd19857ba20ca565a230d4
Quanyu <taoquanyus@foxmail.com>
2020-12-01 01:39:07 +0100

    新手70题之算法双指针

--- leetcode344/src/Solution.java
+++ leetcode344/src/Solution.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public void reverseString(char[] s){//双指针
+    public void reverseString(char[] s){//双指针我的代码
         int i=0,j=s.length-1;
         while(i<j){
             char temp;
@@ -9,4 +9,14 @@ public class Solution {
             ++i;--j;
         }
     }
+
+    public void reverseString1(char[] s) {//参考答案，更加简洁
+        int n = s.length;
+        for (int left = 0, right = n - 1; left < right; ++left, --right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+        }
+    }
+
 }
