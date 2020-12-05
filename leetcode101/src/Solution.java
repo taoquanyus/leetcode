3cdb7b27872ea78cc1b7033e98d61eb62a3e79c2
Quanyu <taoquanyus@foxmail.com>
2020-12-05 13:00:45 +0100

    新手70题之算法 基础递归练习

---
+++ leetcode101/src/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {//递归的入门思路
+        return check(root, root);
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+}
