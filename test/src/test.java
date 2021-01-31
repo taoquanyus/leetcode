0d3bca2a8f8bd99acd894ae3a4737e29e39304e4
Quanyu <taoquanyus@foxmail.com>
2021-01-31 13:47:16 +0100

    leetcode 顺序刷

--- test/src/test.java
+++ test/src/test.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        int a=38;
-        int b=(int)(Math.pow(10,1));
-        a/=b;
+        int a=-2147483648;
+        int b=-1;
+        a=-a;
         System.out.println(a);
     }
 }
