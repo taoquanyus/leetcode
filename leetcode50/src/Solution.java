034f296251ea3ced5295cc2521b4b295a208c7a8
Quanyu <taoquanyus@foxmail.com>
2021-02-12 10:21:08 +0100

    leetcode 顺序刷

---
+++ leetcode50/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        //方法很巧妙
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+    public double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+}
