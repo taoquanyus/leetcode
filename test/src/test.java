35da19d9fe2b154f01916836c0a0eb4290d6d3e6
Quanyu <taoquanyus@foxmail.com>
2021-02-18 18:43:53 +0100

    leetcode 顺序刷

--- test/src/test.java
+++ test/src/test.java
@@ -2,12 +2,24 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        char a='5';
-        int a1=Integer.valueOf(a);
-        System.out.println(a1);
+        String b="";
+        System.out.println("----------------------");
+        System.out.println(b=="");
+        System.out.println(b.isEmpty());
+        System.out.println(b.length());
+        String c;
+        System.out.println("----------------------");
+        String a=null;
+        System.out.println(a==null);
+        System.out.println(a.length());
+        System.out.println(a.isEmpty());
+//        System.out.println(c==null);
+//        System.out.println(c.isEmpty());
+//        System.out.println(c.length());
+
     }
 
-    private static void change(String[] a) {
-        a[0]="hello2";
-    }
+//    private static void change(String[] a) {
+//        a[0]="hello2";
+//    }
 }
